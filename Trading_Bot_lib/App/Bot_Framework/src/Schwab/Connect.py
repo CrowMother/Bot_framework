68b1f96d1cf9cdade9d4ebede6d3a9a7fb3d57e6
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-01-21 13:18:02 -0600

    first commit

---
+++ Trading_Bot_lib/App/Bot_Framework/src/Schwab/Connect.py
@@ -0,0 +1,5 @@
+
+
+def test_connection():
+    """A basic test function to simulate a connection."""
+    return "Connection successful"
---
+++ Trading_Bot_lib/App/test.py
@@ -0,0 +1,5 @@
+# tests.py
+import pytest as pt
+
+def test_example():
+    assert 1 + 1 == 2
