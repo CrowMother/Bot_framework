80072cdd8b4dbe199fe3f2363190a546cc4cf104
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-01-21 14:08:33 -0600

    restarting setup

--- Trading_Bot_lib/App/Bot_Framework/src/Schwab/Connect.py
+++
@@ -1,5 +0,0 @@
-
-
-def test_connection():
-    """A basic test function to simulate a connection."""
-    return "Connection successful"
--- Trading_Bot_lib/App/test.py
+++
@@ -1,5 +0,0 @@
-# tests.py
-import pytest as pt
-
-def test_example():
-    assert 1 + 1 == 2
