56b65da82119033a0fc2faf8297ee75750115d8f
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-01-22 14:49:38 -0600

    bug, fix account access post creation

--- Bot_App/src/Bot_App/schwab.py
+++ Bot_App/src/Bot_App/schwab.py
@@ -1,18 +1,45 @@
-# src/your_library/schwab.py
 import schwabdev
 import logging
+from . import util
 
 class Schwab_client():
     def __init__(self, account, secret):
         self.client = create_client(account, secret)
+        self.test = "test"
 
     def say_hello(self):
         logging.debug("Hello from Schwab_client!")
 
+    def get_account_positions(self, filter=None, hours=1):
+        logging.info("Getting account positions now")
+        orders = None
+        try:
+            from_date_str = util.get_start_time(hours)
+            to_date_str = util.get_end_time(hours)
 
+            logging.info(f"from_date_str: {from_date_str}, to_date_str: {to_date_str}")
+            logging.info(f"filter: {filter}")
 
+            response = self.client.account_orders_all( 
+                from_date_str,
+                to_date_str,
+                None,  # Optional: set to limit number of results    
+                filter # Optional: Filter by status
+            )
 
-
+            if response.status_code == 200:
+                # Parse the JSON content
+                orders = response.json()
+                return orders
+            else:
+                logging.error(f"Error getting account positions: {response} \n {from_date_str} \n {to_date_str}")
+                return None
+        except Exception as e:
+            logging.error(f"Error getting account positions: {e} response: {response}")
+            return None
 
 def create_client(app_key, app_secret):
-    return schwabdev.Client(app_key, app_secret)
+    logging.info("Initializing Schwabdev client")
+    client = schwabdev.Client(app_key, app_secret)
+    logging.info("Schwabdev client initialized")
+    return client
--- Bot_App/src/Bot_App/util.py
+++ Bot_App/src/Bot_App/util.py
@@ -3,6 +3,7 @@ import os
 import sys
 from dotenv import load_dotenv
 import logging
+from datetime import datetime, timezone, timedelta
 
 def get_secret(key, FILE_PATH=""):
     try:
@@ -11,7 +12,21 @@ def get_secret(key, FILE_PATH=""):
         if value == None:
             #throw error if key not found
             raise Exception ("Key not found / is None")
-        return os.getenv(key)
+        return value
     except Exception as e:
         logging.error(f"Error getting secret from {FILE_PATH}: {e}")
         return None
+    
+
+
+def get_end_time(delta=1):
+    to_date = datetime.now(timezone.utc)
+    from_date = to_date - timedelta(hours=delta)
+    
+    # Format dates as ISO 8601 strings with milliseconds and timezone
+    return from_date.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+     
+def get_start_time(delta=1):
+    to_date = datetime.now(timezone.utc)
+    # Format dates as ISO 8601 strings with milliseconds and timezone
+    return to_date.strftime('%Y-%m-%dT%H:%M:%S.000Z')
