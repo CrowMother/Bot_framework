5c2d8f4783097bde5ac3843b567daccf6e352d6e
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-01-23 09:44:39 -0600

    fixed get account_all

--- Bot_App/src/Bot_App/schwab.py
+++ Bot_App/src/Bot_App/schwab.py
@@ -2,6 +2,8 @@ import schwabdev
 import logging
 from . import util
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Schwab_client():
     def __init__(self, account, secret):
         self.client = create_client(account, secret)
@@ -11,14 +13,10 @@ class Schwab_client():
         logging.debug("Hello from Schwab_client!")
 
     def get_account_positions(self, filter=None, hours=1):
-        logging.info("Getting account positions now")
         orders = None
         try:
-            from_date_str = util.get_start_time(hours)
-            to_date_str = util.get_end_time(hours)
-
-            logging.info(f"from_date_str: {from_date_str}, to_date_str: {to_date_str}")
-            logging.info(f"filter: {filter}")
+            to_date_str = util.get_start_time(hours)
+            from_date_str = util.get_end_time(hours)
 
             response = self.client.account_orders_all( 
                 from_date_str,
@@ -34,12 +32,11 @@ class Schwab_client():
             else:
                 logging.error(f"Error getting account positions: {response} \n {from_date_str} \n {to_date_str}")
                 return None
+            
         except Exception as e:
             logging.error(f"Error getting account positions: {e} response: {response}")
             return None
 
 def create_client(app_key, app_secret):
-    logging.info("Initializing Schwabdev client")
-    client = schwabdev.Client(app_key, app_secret)
-    logging.info("Schwabdev client initialized")
-    return client
+    logging.debug("Initializing Schwabdev client")
+    return schwabdev.Client(app_key, app_secret)
