99dfb850367e2c95cf6beb1efa4abf6f7a324895
CrowMother <herrin8080@gmail.com>
2025-05-19 12:10:23 -0500

    update version number to reflect changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Bot_App",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(include=["Bot_App", "Bot_App.*"]),
     include_package_data=True,
     install_requires=[
