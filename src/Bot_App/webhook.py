ddc479bdb2567c0c48ee280b0b4c40e238c06a11
CrowMother <herrin8080@gmail.com>
2025-12-16 19:05:13 -0600

    update webhook logic

--- src/Bot_App/webhook.py
+++ src/Bot_App/webhook.py
@@ -63,7 +63,8 @@ def format_discord_message(order, channel_id= "", suffix=""):
         if opening_order:
             open_price = extract_execution_price(opening_order)
             if open_price and price:
-                pct_change = ((price - open_price) / open_price) * 100
+                price = float(price)
+                pct_change = (( - open_price) / open_price) * 100
                 emoji = ":chart_with_upwards_trend:" if pct_change >= 0 else ":chart_with_downwards_trend:"
                 gain_line = f"\n{emoji} **{pct_change:+.2f}%** vs open"
     if channel_id != "1220834944767492246":
